b6cfd214e5227c5e4bc19c5887d67978419b73bf
avmochkina <nastyanet@tut.by>
2022-08-02 21:56:52 +0300

    Implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let isStrCorrect = true;  
+    
+        for(let i = 0; i < str.length; i++) {
+            let currentSymbol = str[i];
+        
+            for(let n = 0; n < bracketsConfig.length; n++) {
+
+                if(stack.length && bracketsConfig[n][0] === stack[stack.length-1] && bracketsConfig[n][1] === currentSymbol) {
+                    stack.pop();
+                    break;
+                } else if(bracketsConfig[n][0] === currentSymbol) {
+                    stack.push(currentSymbol);
+                    break;
+                } else if(!stack.length && bracketsConfig[n][1] === currentSymbol) {
+                    isStrCorrect = false;
+                    break;
+                }   
+                
+            }
+
+        } 
+    return stack.length === 0 && isStrCorrect; 
 }
